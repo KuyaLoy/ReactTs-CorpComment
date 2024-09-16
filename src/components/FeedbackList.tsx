59023ad9707a37a07385008d1a3acd46387f91e6
Robin Tapiru <robintapiru0894@gmail.com>
2024-09-16 15:12:31 +0400

    Feedback Items List (Map)

--- src/components/FeedbackList.tsx
+++ src/components/FeedbackList.tsx
@@ -1,9 +1,28 @@
 import FeedbackItem from "./FeedbackItem";
 
+const feedbackItems = [
+  {
+    upvoteCount: 593,
+    badgeLetter: "B",
+    companyName: "ByteGrad",
+    text: "test test test",
+    daysAgo: 4,
+  },
+  {
+    upvoteCount: 593,
+    badgeLetter: "B",
+    companyName: "ByteGrad",
+    text: "test test test",
+    daysAgo: 4,
+  },
+];
+
 export default function FeedbackList() {
   return (
     <ol className="feedback-list">
-      <FeedbackItem />
+      {feedbackItems.map((feedbackItem) => (
+        <FeedbackItem feedbackItem={feedbackItem} />
+      ))}
     </ol>
   );
 }
